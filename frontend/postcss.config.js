8c2f1c6abc287f40719d27bba4cb64434ad4f864
NTChat <Chat9049@gmail.com>
2025-12-03 15:02:56 +0700

    Deploy

--- frontend/postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss'), // Thay đổi này
-    require('autoprefixer'),
-  ],
-}
--- frontend/src/utils/format.js
+++
@@ -1,5 +0,0 @@
-// src/utils/format.js
-export const currency = (n) => {
-  const v = Number(n);
-  return (Number.isFinite(v) ? v : 0).toLocaleString("vi-VN") + " ₫";
-};
